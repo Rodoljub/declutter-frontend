c0d65a63f2e5d3e3bac9a3b6f14bf6e913785e0c
Rodoljub Tanasic <rodoljub.tanasic@outlook.com>
2025-08-15 16:34:29 +0200

    feat(items): add EditItemComponent with pre-filled form and update functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginComponent } from './features/auth/login/login.component';
 import { ItemListComponent } from './features/items/item-list/item-list.component';
 import { authGuard } from './core/auth/auth.guard';
 import { AddItemComponent } from './features/items/add-item/add-item.component';
+import { EditItemComponent } from './features/items/edit-item/edit-item.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'items', component: ItemListComponent, canActivate: [authGuard] },
   { path: 'add-item', component: AddItemComponent, canActivate: [authGuard] },
+   { path: 'edit-item/:id', component: EditItemComponent, canActivate: [authGuard] },
   { path: '', redirectTo: 'login', pathMatch: 'full' }, // ✅ pathMatch is "full"
 ];
